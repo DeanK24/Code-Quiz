f31ceae991452f52b7f22ee51127782056ed3ba3
Dean <deank242@gmail.com>
2022-03-16 21:19:44 -0400

    updated css and js file

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: "Which of the following element is responsible for making the text italic in HTML?",
+        a: "<i>",
+        b: "<italic>",
+        c: "<it>",
+        d: "<pre>",
+        correct: "a",
+    },
+    {
+        question: "Which HTML attribute is used to define the internal stylesheet?",
+        a: "<style>",
+        b: "style",
+        c: "<link>",
+        d: "<script>",
+        correct: "a",  
+    },
+    {
+        question: "Which of the following property is used as the shorthand property for the padding properties?",
+        a: "padding-left",
+        b: "padding-right",
+        c: "padding",
+        d: "All of the above",
+        correct: "c",
+    },
+    {
+        question: "The CSS property used to specify whether the text is written in the horizontal or vertical direction?",
+        a: "writing-mode",
+        b: "text-indent",
+        c: "word-break",
+        d: "None of the above",
+        correct: "a",
+    },
+    {
+        question: "Which oone of the following also known as Conditional Expression?",
+        a: "Alternative to if-else",
+        b: "Switch statement",
+        c: "If-then-else statement",
+        d: "immediate if",
+        correct: "d",
+    }
+]
